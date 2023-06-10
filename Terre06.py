88468e00da9aa45be96c131d4539d047a5c57917
Darstt <135744074+Darstt@users.noreply.github.com>
2023-06-10 22:09:25 +0200

    Update Terre06.py

--- Terre06.py
+++ Terre06.py
@@ -1,3 +1,6 @@
-chaine = input("Entrez une chaîne de caractères : ")
-inverse = chaine[::-1]
-print("Inverse de la chaîne :", inverse)
+try:
+    chaine = input("Entrez une chaîne de caractères : ")
+    inverse = chaine[::-1]
+    print("Inverse de la chaîne :", inverse)
+except:
+    print("Une erreur s'est produite. Assurez-vous d'entrer une chaîne de caractères valide.")
