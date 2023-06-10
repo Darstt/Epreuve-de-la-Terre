e90a9f6e851fe0b208cbbca33d0a05bd4e6965bc
Darstt <135744074+Darstt@users.noreply.github.com>
2023-06-10 17:23:00 +0200

    Create Terre03.py

---
+++ Terre03.py
@@ -0,0 +1,17 @@
+# introduire une lettre et la definir comme a
+
+a = input("Introduire une lettre:")
+
+# definir z
+
+z = 123
+
+# transformer valeur de a en ascii
+
+b = ord(a)
+
+# afficher l'alphabet
+
+for i in range (b, z):
+	print(chr(i), end ="")
+print ("")
